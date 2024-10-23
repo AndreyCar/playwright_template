6ac03fdaab718aca4a7a3e162ea2520f2bb7dea2
AndriiCar <kravchenko.svg@gmail.com>
2024-10-23 23:00:30 +0300

    Update test name

--- tests/01.login.positive.spec.ts
+++ tests/01.login.positive.spec.ts
@@ -7,7 +7,7 @@ test.describe('Login', () => {
     // test.afterEach(async ({}) => {});
     // test.afterAll(async ({}) => {});
 
-    test('Should log in with valid credentials', async ({ loginPage, inventoryPage }) => {
+    test('should log in with valid credentials', async ({ loginPage, inventoryPage }) => {
         await loginPage.open();
         await loginPage.login({ username: process.env.LOGIN_USERNAME, password: process.env.LOGIN_PASSWORD });
         await expect(inventoryPage.btnExtendSidebarMenu).toBeVisible();
